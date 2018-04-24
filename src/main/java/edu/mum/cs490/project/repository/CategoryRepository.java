69e4c7ecd009833c6ecc1c2983b4e1d0747f6f7a
chanpisethchea <chpiseth9@gmail.com>
2018-04-24 12:02:51 -0500

    Crated CategoryRepository

---
+++ src/main/java/edu/mum/cs490/project/repository/CategoryRepository.java
@@ -0,0 +1,25 @@
+package edu.mum.cs490.project.repository;
+
+import edu.mum.cs490.project.domain.Category;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by ChanPiseth on 4/24/2018
+ */
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+
+    List<Category> findAllByMainCategoryName(String mainCategoryName);
+
+    List<Category> findAllBySubCategoryName(String subCategoryName);
+
+    @Query(value = "SELECT DISTINCT sub_category_name from category" , nativeQuery = true)
+    List<String> findAllSubCategoryName();
+
+    @Query(value = "SELECT DISTINCT main_category_name from category" , nativeQuery = true)
+    List<String> findAllMainCategoryName();
+}

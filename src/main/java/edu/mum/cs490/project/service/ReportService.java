2697e0d7aaecf6b0ca19fe8e5d2c9e82eef0efe9
Hong Ha <thuyhong.ip@gmail.com>
2018-04-24 10:01:38 -0500

    start create report

---
+++ src/main/java/edu/mum/cs490/project/service/ReportService.java
@@ -0,0 +1,12 @@
+package edu.mum.cs490.project.service;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.*;
+
+
+@Transactional
+public interface ReportService {
+	public List<Object> getReportResultList();
+
+}
---
+++ src/main/java/edu/mum/cs490/project/service/impl/ReportServiceImpl.java
@@ -0,0 +1,17 @@
+package edu.mum.cs490.project.service.impl;
+
+import java.util.List;
+
+import edu.mum.cs490.project.service.ReportService;
+
+public class ReportServiceImpl implements ReportService{
+
+//	private OrderService orderSerice;
+	
+	@Override
+	public List<Object> getReportResultList() {
+		
+		return null;
+	}
+
+}

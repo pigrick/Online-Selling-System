9e167b6f1d45482962f53163a2a04aff628520de
Hong Ha <thuyhong.ip@gmail.com>
2018-04-29 20:38:04 -0500

    Multiple choice_UI

--- src/main/java/edu/mum/cs490/project/controller/ReportController.java
+++ src/main/java/edu/mum/cs490/project/controller/ReportController.java
@@ -7,7 +7,6 @@ import edu.mum.cs490.project.service.CategoryService;
 import edu.mum.cs490.project.service.OrderDetailService;
 import edu.mum.cs490.project.service.VendorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -31,9 +30,6 @@ public class ReportController {
     @Autowired
     private CategoryService categoryService;
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
     @GetMapping(value = "/reportFilter")
     public String adminReportLoading(Model model) {
         List<Vendor> vendorList = vendorService.getAll();
@@ -51,24 +47,4 @@ public class ReportController {
 
         return "report/reportFilter";
     }
-
-//    @GetMapping(value = "/vendorReport")
-//    public String vendorReportLoading(Model model) {
-//        List<Category> categoryList = categoryService.getAllCategory();
-//        List<String> categoryListNames = new ArrayList<>();
-//        for (Category c : categoryList) {
-//            String item = c.getName();
-//            System.out.print("Test category" + item);
-//            categoryListNames.add(item);
-//        }
-//        model.addAttribute("categories", categoryListNames);
-//        return "report/vendorReport";
-//    }
-
-//    @PostMapping(value = "/vendorReport")
-//    public String vendorReportExport(Model model, @RequestParam String from, @RequestParam String to, @RequestParam String category) {
-//        return "report/vendorExport";
-//    }
-
-
 }

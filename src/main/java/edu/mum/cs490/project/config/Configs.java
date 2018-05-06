fa3b5d7c62600316a3b4bce3f15c6b4096715077
Erdenebayar <tomjaal@gmail.com>
2018-05-06 15:35:40 -0500

    Seperated MockConnection Config

--- src/main/java/edu/mum/cs490/project/config/Configs.java
+++ src/main/java/edu/mum/cs490/project/config/Configs.java
@@ -1,9 +1,5 @@
 package edu.mum.cs490.project.config;
 
-import edu.mum.cs490.project.utils.AESConverter;
-import edu.mum.cs490.project.utils.HttpSender;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -14,7 +10,8 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({
         WebSecurityConfig.class,
-        WebConfig.class
+        WebConfig.class,
+        MockConnectionConfig.class
 })
 @ComponentScan({
         "edu.mum.cs490.project.repository",
@@ -23,32 +20,4 @@ import org.springframework.context.annotation.Import;
         "edu.mum.cs490.project.utils",
 })
 public class Configs {
-
-    @Value("${mock.api.url}")
-    private String mockApiUrl;
-    @Value("${mock.api.user}")
-    private String mockApiUser;
-    @Value("${mock.api.pass}")
-    private String mockApiPass;
-    @Value("${mock.api.content.type}")
-    private String mockApiContentType;
-    @Value("${mock.api.conn.time}")
-    private Integer mockApiConnTime;
-    @Value("${mock.api.read.time}")
-    private Integer mockApiReadTime;
-    @Value("${mock.api.secret.key}")
-    private String mockApiSecretKey;
-
-    @Bean
-    public HttpSender httpSender() {
-        System.out.println("HTTP SENDER is created");
-        return new HttpSender(mockApiUrl, mockApiUser, mockApiPass, mockApiContentType, mockApiConnTime, mockApiReadTime);
-    }
-
-    @Bean
-    public AESConverter aesConverter() {
-        System.out.println("AES is created");
-        return new AESConverter(mockApiSecretKey);
-    }
-
 }
